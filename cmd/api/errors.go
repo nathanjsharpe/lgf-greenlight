1c1fbb76b76cef639547121c8a0582db2d241cdc
Nathan Sharpe <nathan@nathanjsharpe.com>
2024-03-06 08:28:20 -0500

    Finish chapter 10: add rate limiter

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -52,3 +52,8 @@ func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Requ
 	message := "unable to update record due to edit conflict, please try again"
 	app.errorResponse(w, r, http.StatusConflict, message)
 }
+
+func (app *application) rateLimitExceededResponse(w http.ResponseWriter, r *http.Request) {
+	message := "woah there, slow down"
+	app.errorResponse(w, r, http.StatusTooManyRequests, message)
+}
