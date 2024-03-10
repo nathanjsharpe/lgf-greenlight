b792043954808551df490c27daaf12305a008546
Nathan Sharpe <nathan@nathanjsharpe.com>
2024-03-09 23:36:32 -0500

    Work through 18.3: metrics using expvar

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"database/sql"
+	"expvar"
 	"flag"
 	"greenlight.nathanjsharpe.com/internal/data"
 	"greenlight.nathanjsharpe.com/internal/mailer"
 	"log/slog"
 	"os"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -89,6 +91,17 @@ func main() {
 	defer db.Close()
 
 	logger.Info("database connection pool established")
+
+	expvar.NewString("version").Set(version)
+	expvar.Publish("goroutines", expvar.Func(func() any {
+		return runtime.NumGoroutine()
+	}))
+	expvar.Publish("database", expvar.Func(func() any {
+		return db.Stats()
+	}))
+	expvar.Publish("timestamp", expvar.Func(func() any {
+		return time.Now().Unix()
+	}))
 
 	app := &application{
 		config: cfg,
